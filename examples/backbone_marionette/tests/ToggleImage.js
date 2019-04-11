b5041d800307a725ef541b2cb516d7d7cc427662
Vicente Amoros <v.amoros@quintgroup.com>
2019-04-11 13:02:26 +0200

    SD-20 Continuous Testing

--- examples/backbone_marionette/tests/ToggleImage.js
+++ examples/backbone_marionette/tests/ToggleImage.js
@@ -10,17 +10,26 @@ var driver = new Builder()
 
 driver.get('http://localhost:8080')
     .then(_ => driver.wait(driver.getTitle()))
-    .then(_ => driver.wait(driver.findElement(By.name('image')).then(function(){
+    .then(_ => driver.wait(driver.findElement(By.name('image')).then(
+        function(){
             initialImage = driver.findElement(By.name('image'));
             initialImage.getAttribute('src').then(function(src) {initialImageSrc=src;});
-
-        })))
+        },
+        function (err) {
+            if (err.name === "NoSuchElementError"){
+                console.log("Test failed");
+            }
+        }
+    )))
     .then(_ => driver.findElement(By.name('new-todo')).sendKeys('Tarea 1'))
     .then(_ => driver.findElement(By.name('new-todo')).sendKeys(Key.RETURN))
-    .then(_ => driver.sleep(2000).then(function(){
-        finalImage = driver.findElement(By.name('image'));
-        finalImage.getAttribute('src').then(function(src) {finalImageSrc=src;});
-    }))
+    .then(_ => driver.sleep(2000).then(
+        function(){
+            finalImage = driver.findElement(By.name('image'));
+            finalImage.getAttribute('src').then(function(src) {finalImageSrc=src;});
+            }
+        )   
+    )
     .then(_ =>driver.then(function(){
         if(finalImageSrc!== initialImageSrc){
             console.log(initialImageSrc);
