5a01dc3081d1e85b9c237a3b34ca420d3c87efb1
Vicente Amoros <v.amoros@quintgroup.com>
2019-04-18 12:14:03 +0200

    Ajustado el caso de prueba en PhantomJS para que lance un error si el web server no está disponible

--- examples/backbone_marionette/tests/ToggleImage_Phantom.js
+++ examples/backbone_marionette/tests/ToggleImage_Phantom.js
@@ -4,6 +4,11 @@ const phantom = require('phantom');
     const instance = await phantom.create();
     const page = await instance.createPage();
     const status = await page.open('http://localhost:8080');
+    
+    if (status !== "success") {
+        throw new Error("The web server is not available");
+    }
     console.log(status);
+    
     await instance.exit();
 }());
